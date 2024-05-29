6fd5958d2f677102e14712c90081787fee1886ce
bug-sys <40689080+bug-sys@users.noreply.github.com>
2024-05-30 04:48:31 +0700

    Delete Armbian/sources directory

--- Armbian/sources/emmc_autoscript.cmd
+++
@@ -1,6 +0,0 @@
-setenv env_addr "0x11400000"
-setenv kernel_addr "0x14000000"
-setenv initrd_addr "0x15000000"
-setenv dtb_mem_addr "0x11800000"
-setenv boot_start booti ${kernel_addr} ${initrd_addr} ${dtb_mem_addr}
-if fatload mmc 1 ${kernel_addr} zImage; then if fatload mmc 1 ${initrd_addr} uInitrd; then if fatload mmc 1 ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize}; fi; if fatload mmc 1 ${dtb_mem_addr} ${dtb_name}; then run boot_start;fi;fi;fi;
