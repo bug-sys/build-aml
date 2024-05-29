6bacf5bdb72ce2e662115c5f59e210bada59be26
bug-sys <40689080+bug-sys@users.noreply.github.com>
2024-05-30 05:01:58 +0700

    Update build.sh

--- Armbian/build.sh
+++ Armbian/build.sh
@@ -2,24 +2,36 @@
 #set -x
 
 # Memproses jenis chipset/SoC yang ingin dibuat
-echo -e "\e[33mPilih jenis chipset/SoC yang ingin Anda buat (s805/s905):\e[0m"
-read jenis
+echo -e "\e[33mPilih jenis chipset/SoC yang ingin Anda buat:\e[0m"
+echo "1. AMLs805"
+echo "2. AMLs905"
+read -p "Masukkan nomor chipset yang ingin Anda buat (1/2): " nomor
+
+# Validasi pilihan pengguna
+case "$nomor" in
+    1)
+        jenis="s805"
+        ;;
+    2)
+        jenis="s905"
+        ;;
+    *)
+        echo -e "\e[31mPilihan tidak valid. Silakan pilih nomor 1 untuk AMLs805 atau nomor 2 untuk AMLs905.\e[0m" 
+        exit 1
+        ;;
+esac
 
 # Membuat direktori untuk menyimpan hasil
 HASIL=hasil
 mkdir -p $HASIL
 
-# Melakukan pemrosesan berdasarkan pilihan pengguna
+# Menentukan CHIPSET berdasarkan jenis yang dipilih
 case "$jenis" in
     s805)
         CHIPSET=s805
         ;;
     s905)
         CHIPSET=s905
-        ;;
-    *)
-        echo -e "\e[31mPilihan tidak valid. Silakan pilih antara 's805' atau 's905'.\e[0m" 
-        exit 1
         ;;
 esac
 
@@ -35,6 +47,7 @@ process_chipset() {
     fi
 }
 
+# Memproses setiap jenis file yang diperlukan untuk chipset yang dipilih
 process_chipset "aml_autoscript"
 process_chipset "${CHIPSET}_autoscript"
 process_chipset "emmc_autoscript"
