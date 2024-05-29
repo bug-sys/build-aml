939d4b0febe48aec1e7cc3aba3d52e37d98c2fde
bug-sys <40689080+bug-sys@users.noreply.github.com>
2024-05-30 03:51:00 +0700

    Update build.sh

--- Armbian/build.sh
+++ Armbian/build.sh
@@ -2,9 +2,33 @@
 #set -x
 Scripts=hasil
 Sources=sources
+ResultFolder=result
 
 mkdir -p $Scripts
+mkdir -p $ResultFolder
+
+echo "Pilih jenis script yang ingin dibuat:"
+echo "1. s805_autoscript"
+echo "2. s905_autoscript"
+read -p "Masukkan pilihan (1 atau 2): " choice
+
+case $choice in
+    1)
+        script_name="s805_autoscript"
+        ;;
+    2)
+        script_name="s905_autoscript"
+        ;;
+    *)
+        echo "Pilihan tidak valid. Keluar."
+        exit 1
+        ;;
+esac
+
 mkimage -C none -A arm -T script -d $Sources/aml_autoscript.cmd $Scripts/aml_autoscript
-mkimage -C none -A arm -T script -d $Sources/s805_autoscript.cmd $Scripts/s805_autoscript
+mkimage -C none -A arm -T script -d $Sources/"$script_name".cmd $Scripts/"$script_name"
 mkimage -C none -A arm -T script -d $Sources/emmc_autoscript.cmd $Scripts/emmc_autoscript
+
+mv $Scripts/* $ResultFolder  # Pindahkan semua file ke folder hasil
+
 echo "selesai"
