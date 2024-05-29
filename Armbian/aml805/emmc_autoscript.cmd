122d69fa7808aa0d4254db615b758aa544d90474
bug-sys <40689080+bug-sys@users.noreply.github.com>
2024-05-30 05:39:25 +0700

    Delete Armbian/aml805 directory

--- Armbian/aml805/emmc_autoscript.cmd
+++
@@ -1,7 +0,0 @@
-setenv env_addr "0x11400000"
-setenv dtb_addr "0x11800000"
-setenv kernel_addr "0x14000000"
-setenv initrd_addr "0x15000000"
-setenv boot_start bootm ${kernel_addr} ${initrd_addr} ${dtb_addr}
-if fatload mmc 1 ${env_addr} uEnv.txt; then env import -t ${env_addr} ${filesize}; setenv bootargs ${APPEND}; video dev open ${VMODE}; if fatload mmc 1 ${kernel_addr} ${LINUX}; then if fatload mmc 1 ${initrd_addr} ${INITRD}; then if fatload mmc 1 ${dtb_addr} ${FDT}; then run boot_start; fi; fi; fi; fi;
-#video dev open 1080P50HZ
