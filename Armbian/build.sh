9d2872ee00e7aeda88c3b61d668f4a218483038c
bug-sys <40689080+bug-sys@users.noreply.github.com>
2024-05-30 04:48:12 +0700

    Update build.sh

--- Armbian/build.sh
+++ Armbian/build.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 #set -x
-Scripts=hasil
-Sources=sources
-ResultFolder=result
 
-mkdir -p $Scripts
-mkdir -p $ResultFolder
+# Memproses jenis chipset/SoC yang ingin dibuat
+echo -e "\e[33mPilih jenis chipset/SoC yang ingin Anda buat (s805/s905):\e[0m"
+read jenis
 
-echo "Pilih jenis script yang ingin dibuat:"
-echo "1. s805_autoscript"
-echo "2. s905_autoscript"
-read -p "Masukkan pilihan (1 atau 2): " choice
+# Membuat direktori untuk menyimpan hasil
+HASIL=hasil
+mkdir -p $HASIL
 
-case $choice in
-    1)
-        script_name="s805_autoscript"
+# Melakukan pemrosesan berdasarkan pilihan pengguna
+case "$jenis" in
+    s805)
+        CHIPSET=s805
         ;;
-    2)
-        script_name="s905_autoscript"
+    s905)
+        CHIPSET=s905
         ;;
     *)
-        echo "Pilihan tidak valid. Keluar."
+        echo -e "\e[31mPilihan tidak valid. Silakan pilih antara 's805' atau 's905'.\e[0m" 
         exit 1
         ;;
 esac
 
-mkimage -C none -A arm -T script -d $Sources/aml_autoscript.cmd $Scripts/aml_autoscript
-mkimage -C none -A arm -T script -d $Sources/"$script_name".cmd $Scripts/"$script_name"
-mkimage -C none -A arm -T script -d $Sources/emmc_autoscript.cmd $Scripts/emmc_autoscript
+# Menjalankan perintah mkimage untuk chipset yang dipilih
+process_chipset() {
+    local script="$1"
+    echo -e "\e[34mMemproses file $script...\e[0m" 
+    if mkimage -C none -A arm -T script -d "$CHIPSET/$script.cmd" "$HASIL/$script" >/dev/null 2>&1; then
+        echo -e "\e[32mSelesai\e[0m"
+    else
+        echo -e "\e[31mGagal memproses $script\e[0m"
+        exit 1
+    fi
+}
 
-mv $Scripts/* $ResultFolder  # Pindahkan semua file ke folder hasil
+process_chipset "aml_autoscript"
+process_chipset "${CHIPSET}_autoscript"
+process_chipset "emmc_autoscript"
 
-echo "selesai"
+echo -e "\e[32mSEMUA PROSES SUDAH SELESAI !!!\e[0m"
