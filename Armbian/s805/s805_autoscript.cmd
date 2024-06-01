f7c1bc51486bad6e65b7369debc66ecf0b48ba87
bug-sys <40689080+bug-sys@users.noreply.github.com>
2024-06-02 01:29:25 +0700

    Update s805_autoscript.cmd

--- Armbian/s805/s805_autoscript.cmd
+++ Armbian/s805/s805_autoscript.cmd
@@ -1,5 +1,6 @@
 setenv env_addr "0x11400000"
+setenv dtb_addr "0x11800000"
 setenv kernel_addr "0x14000000"
 setenv initrd_addr "0x15000000"
 setenv boot_start bootm ${kernel_addr} ${initrd_addr} ${dtb_addr}
-if fatload mmc 0 ${kernel_addr} uImage; then if fatload mmc 0 ${initrd_addr} uInitrd; then if fatload mmc 0 ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize};fi; if fatload mmc 0 ${dtb_addr} ${dtb_name}; then run boot_start; else store dtb read ${dtb_addr}; run boot_start;fi;fi;fi;
+if fatload mmc 0 ${env_addr} uEnv.ini; then env import -t ${env_addr} ${filesize}; setenv bootargs ${APPEND}; video dev open ${VMODE}; if fatload mmc 0 ${kernel_addr} ${LINUX}; then if fatload mmc 0 ${initrd_addr} ${INITRD}; then if fatload mmc 0 ${dtb_addr} ${FDT}; then run boot_start; fi; fi; fi; fi;
