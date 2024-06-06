e2b4b758b9ac56c7cdae3ed7eee4bf1a08a8fce9
bug-sys <40689080+bug-sys@users.noreply.github.com>
2024-06-07 01:10:24 +0700

    Delete Armbian directory

--- Armbian/s805/aml_autoscript.cmd
+++
@@ -1,9 +0,0 @@
-defenv
-setenv bootcmd 'run start_autoscript; run storeboot;'
-setenv start_autoscript 'if mmcinfo; then run start_mmc_autoscript; fi; run start_emmc_autoscript;'
-setenv start_emmc_autoscript 'if fatload mmc 1 11000000 emmc_autoscript; then autoscr 11000000; fi;'
-setenv start_mmc_autoscript 'if fatload mmc 0 11000000 s805_autoscript; then autoscr 11000000; fi;'
-setenv upgrade_step '2'
-saveenv
-sleep 1
-reboot
